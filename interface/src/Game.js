1df7a1f597909938b5f0e13148873c7ee7abbff9
Simeon Tica <xorythebest@gmail.com>
2023-11-04 23:54:34 +0200

    small edits to cards

--- interface/src/Game.js
+++ interface/src/Game.js
@@ -3,10 +3,14 @@ import './styles/Game.css';
 
 const Game = () => {
 
-    const [card, setCard] = useState('');
+    const [card, setCard] = useState('Choose a card');
 
     return ( 
         <div>
+            {/* aici trebuie facut un template care sa incarca el div in functie de fetch ul pe care
+             il facem si cate intrebari avem nu o sa fie asa 
+             plus ca trebuie sa avem un meniu unde sa selectam jucatorii
+             */}
             <div class="top-thinggy">{ card }</div>
             <div className='card-container'>
                 <div className='card' onMouseOver={() => setCard('card 1')}>card 1</div>
