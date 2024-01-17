4b0d07d12be8e3e7a7e65d5518284da5b3c46a43
Simeon Tica <xorythebest@gmail.com>
2024-01-17 12:57:30 +0200

    changing ip adress

--- interface/src/Join.js
+++ interface/src/Join.js
@@ -24,8 +24,8 @@ const Join = ({ name, room }) => {
         room: roomNumber
     };
 
-    const socket = new WebSocket("ws://0.0.0.0:8080/ws");
-    const socketStart = new WebSocket("ws://0.0.0.0:8080/ws/startgame");    
+    const socket = new WebSocket("ws://localhost:8080/ws");
+    const socketStart = new WebSocket("ws://localhost:8080/ws/startgame");    
 
     socketStart.addEventListener("open", event => {
         socketStart.send(JSON.stringify(dataStart));
